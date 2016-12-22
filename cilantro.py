48dd6b034765b4bd277a0fa38b84fc4618f43191
onibrow <seiya.ono12@gmail.com>
2016-12-22 04:37:43 +0000

    Add refresing capability

--- cilantro.py
+++ cilantro.py
@@ -1,27 +1,31 @@
 from entity import *
 import pygame
 import math
+import random
 
 
 class cilantro(entity):
 
-    def __init__(self, name, x, y, img1, img2, img3, vel, playerPos):
+    def __init__(self, name, x, y, img1, img2, img3, vel, playerPos, maxX, maxY):
         entity.__init__(self, name, x, y, img1)
         self.img1 = pygame.image.load(img1)
         self.img2 = pygame.image.load(img2)
         self.img3 = pygame.image.load(img3)
+        self.maxX = maxX
+        self.maxY = maxY
         self.velocity = vel
+        self.origX = self.x
+        self.origY = self.y
         self.angle = 0
         self.dest = playerPos
         self.xMove, self.ymove = 0, 0
         self.which_image = 0
         x = self.dest[0] - self.getPos()[0] + 0.1
         y = self.dest[1] - self.getPos()[1] + 0.1
+        norm = math.sqrt(x * x + y * y)
         rads = math.atan2(-y,x)
         rads %= 2*math.pi
-        self.angle = math.degrees(rads)
-        x, y = x / max(x, y), y / max(x,y)
-        self.angle = 90
+        self.angle = 180
         print(self.angle)
         self.xMove = x * self.velocity
         self.yMove = y * self.velocity
@@ -40,5 +44,18 @@ class cilantro(entity):
         elif self.which_image % 12 == 0:
             self.img = pygame.transform.rotate(self.img3, -self.angle)
 
-    def setPos(self, x, y):
-        self.x, self.y = x, y
+    def refresh(self, playerPos):
+        #print(self.x, self.maxX, self.y, self.maxY)
+        if (self.x < 0 or self.x > self.maxX or self.y < 0 or self.y > self.maxY):
+            print("refresH")
+            self.x, self.y = self.origX, self.origY
+            self.velocity = random.randrange(2, 6)
+            print(self.velocity)
+            x = self.dest[0] - self.getPos()[0] + 0.1
+            y = self.dest[1] - self.getPos()[1] + 0.1
+            norm = math.sqrt(x * x + y * y)
+            x, y = x / norm, y / norm
+            self.xMove = x * self.velocity
+            self.yMove = y * self.velocity
+
+
